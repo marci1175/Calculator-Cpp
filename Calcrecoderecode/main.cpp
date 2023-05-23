25dcb8285e4c8877a1affa441ea98aa750817d1d
Marci <128537619+marci1175@users.noreply.github.com>
2023-05-23 22:03:58 +0200

    Fixed ! usage

--- Calcrecoderecode/main.cpp
+++ Calcrecoderecode/main.cpp
@@ -42,7 +42,7 @@ public:
 		veg = privfact(first);
 		return veg;
 	};
-	
+
 private:
 	//pow
 	double privpow(double first, double second) {
@@ -63,136 +63,137 @@ private:
 		factv.push_back(1);
 		for (size_t i = 1; i < first + 1; i++)
 		{
-			veg = factv[i- 1] * i;
+			veg = factv[i - 1] * i;
 			factv.push_back(veg);
 		}
 		return veg;
 	}
-	
+
 };
 
 int main() {
-	double veg = 0;
-	szamitas3 lv1;
-	szamitas2 lv2;
-	sciszamitas2 scilv2;
+	while(true){
+		double veg = 0;
+		szamitas3 lv1;
+		szamitas2 lv2;
+		sciszamitas2 scilv2;
 
-	std::vector <std::string> user_input;
-	std::vector <double> num;
-	std::vector <std::string> exp;
+		std::vector <std::string> user_input;
+		std::vector <double> num;
+		std::vector <std::string> exp;
 
-	std::cout << "|: ";
-	//ss
+		std::cout << "|: ";
+		//ss
 
-	std::string user_inputTEMP, name;
-	std::getline(std::cin, user_inputTEMP);
-	std::stringstream ss(user_inputTEMP);
-	while (ss >> name) {
-		user_input.push_back(name);
+		std::string user_inputTEMP, name;
+		std::getline(std::cin, user_inputTEMP);
+		std::stringstream ss(user_inputTEMP);
+		while (ss >> name) {
+			user_input.push_back(name);
+		}
+		//separating + and 30
+		for (size_t i = 0; i < user_input.size(); i++)
+		{
+			try
+			{
+				double numT = stof(user_input[i]);
+				num.push_back(numT);
+			}
+			catch (const std::exception&)
+			{
+				exp.push_back(user_input[i]);
+			}
+		}
+		//calculator engine
+		// ! pow
+		//re-backup exp. size
+		int expelozmeny = exp.size();
+		for (size_t i = 0; i < exp.size() + 1; i++)
+		{
+			if (expelozmeny == i or exp.size() < i + 1)
+			{
+				break;
+			}
+			if (exp[0] == "!" or exp[0] == "pow") {
+				i = 0;
+			}
+			//ha 1 akk fail , 0-nal van a +
+			if (exp[i] == "!")
+			{
+				veg = scilv2.fact(num[i]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i);
+				num.insert(num.begin() + i, veg);
+			}
+			else if (exp[i] == "pow")
+			{
+				veg = scilv2.pow(num[i], num[i + 1]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i, num.begin() + (i + 2));
+				num.insert(num.begin() + i, veg);
+			}
+		}
+		// : *
+		//re-backup exp. size
+		expelozmeny = exp.size();
+		for (size_t i = 0; i < exp.size() + 1; i++)
+		{
+			if (expelozmeny == i)
+			{
+				break;
+			}
+			if (exp[0] == ":" or exp[0] == "/" or exp[0] == "*") {
+				i = 0;
+			}
+			if (exp[i] == ":" or exp[i] == "/")
+			{
+				veg = lv2.oszt(num[i], num[i + 1]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i, num.begin() + (i + 2));
+				num.insert(num.begin() + i, veg);
+			}
+			else if (exp[i] == "*")
+			{
+				veg = lv2.szor(num[i], num[i + 1]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i, num.begin() + (i + 2));
+				num.insert(num.begin() + i, veg);
+			}
+		}
+		//+ -
+		//re-backup exp. size
+		expelozmeny = exp.size();
+		for (size_t i = 0; i < exp.size() + 1; i++)
+		{
+			if (expelozmeny == i)
+			{
+				break;
+			}
+			if (exp[0] == "+" or exp[0] == "-") {
+				i = 0;
+			}
+			if (exp[i] == "-")
+			{
+				veg = lv1.kivo(num[i], num[i + 1]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i, num.begin() + (i + 2));
+				num.insert(num.begin() + i, veg);
+			}
+			else if (exp[i] == "+")
+			{
+				veg = lv1.ossz(num[i], num[i + 1]);
+				exp.erase(exp.begin() + i);
+				num.erase(num.begin() + i, num.begin() + (i + 2));
+				num.insert(num.begin() + i, veg);
+			}
+
+		}
+
+
+
+		//print out veg
+		std::cout << "Answer : " << veg << std::endl;
+		system("pause");
+		system("cls");
 	}
-	//separating + and 30
-	for (size_t i = 0; i < user_input.size(); i++)
-	{
-		try
-		{
-			double numT = stof(user_input[i]);
-			num.push_back(numT);
-		}
-		catch (const std::exception&)
-		{
-			exp.push_back(user_input[i]);
-		}
-	}
-	//calculator engine
-	// ! pow
-	//re-backup exp. size
-	int expelozmeny = exp.size();
-	for (size_t i = 0; i < exp.size() + 1; i++)
-	{
-		if (expelozmeny == i)
-		{
-			break;
-		}
-		if (exp[0] == "!" or exp[0] == "pow") {
-			i = 0;
-		}
-		if (exp[i] == "!")
-		{
-			veg = scilv2.fact(num[i]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-		else if (exp[i] == "pow")
-		{
-			veg = scilv2.pow(num[i], num[i + 1]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-	}
-	// : *
-	//re-backup exp. size
-	expelozmeny = exp.size();
-	for (size_t i = 0; i < exp.size() + 1; i++)
-	{
-		if (expelozmeny == i)
-		{
-			break;
-		}
-		if (exp[0] == ":" or exp[0] == "/" or exp[0] == "*") {
-			i = 0;
-		}
-		if (exp[i] == ":" or exp[i] == "/")
-		{
-			veg = lv2.oszt(num[i], num[i + 1]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-		else if (exp[i] == "*")
-		{
-			veg = lv2.szor(num[i], num[i + 1]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-	}
-	//+ -
-	//re-backup exp. size
-	expelozmeny = exp.size();
-	for (size_t i = 0; i < exp.size() + 1; i++)
-	{
-		if (expelozmeny == i)
-		{
-			break;
-		}
-		if (exp[0] == "+" or exp[0] == "-") {
-			i = 0;
-		}
-		if (exp[i] == "-")
-		{
-			veg = lv1.kivo(num[i], num[i + 1]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-		else if (exp[i] == "+")
-		{
-			veg = lv1.ossz(num[i], num[i + 1]);
-			exp.erase(exp.begin() + i);
-			num.erase(num.begin() + i, num.begin() + (i + 2));
-			num.insert(num.begin() + i, veg);
-		}
-		
-	}
-
-
-
-	//print out veg
-	std::cout << "Answer : " << veg << std::endl;
-	system("pause");
-	system("cls");
-	main();
-	return 0;
 }
